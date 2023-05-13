1aaa69384c6a37c60313aa2b68f86f5bf3c0dda0
Tolfin <82317354+tolfinn@users.noreply.github.com>
2023-05-13 18:54:46 +0800

    Delete qianduan/src directory

--- qianduan/src/store/actions.js
+++
@@ -1,7 +0,0 @@
-//调用mutations
-export const setUser = ({commit},user) => {
-    commit("userStatus",user);
-}
-export const setToken = ({commit},token) =>{
-    commit("setToken",token);
-}
--- qianduan/src/store/getters.js
+++
@@ -1,3 +0,0 @@
-export const isLogin = state => state.isLogin;
-export const userName = state => state.userName;
-export const roleName = state => state.roleName;
